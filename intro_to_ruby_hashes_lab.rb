e1744b25fb6b88468633b6fc7932df27e711121a
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-10-04 16:40:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,11 +1,9 @@
 def base_hash
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
 	{railroads: {}}
 end
 
 def monopoly_with_second_tier
-{}
+	{railroads: {pieces: ""}}
 end
 
 def monopoly_with_third_tier
