d7d10ed8107d2b8b8aba74f385ae48256cf4ecf6
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-10-04 17:05:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,6 +34,6 @@ def monopoly_with_fourth_tier
 				{pennsylvania_railroad:{"mortgage_value" => "$200"},
 				 b_and_o_railroad:{"mortgage_value" => "$400"},
 				 reading_railroad:{"mortgage_value" => "$100"},
-				 shortline_railroad:{"mortgage_value" => "$400"}}
+				 shortline_railroad:{"mortgage_value" => "$800"}}
 				 }}
 end
